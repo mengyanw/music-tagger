293bae85bbbf51505eaa8bb9eb3b071a66660cb5
wumengyan <mengyanw@umich.edu>
2023-03-13 15:19:48 -0400

    feat: Deploy React app to GitHub Pages

---
+++ precache-manifest.91155a520d4c6fce98ca692f4edf1a5f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c25c8d4ebe3796eaeef5fbc5e71e7758",
+    "url": "/music-tagger/index.html"
+  },
+  {
+    "revision": "2489d5a46d8737d451ab",
+    "url": "/music-tagger/static/js/2.53cd735d.chunk.js"
+  },
+  {
+    "revision": "3ce4c6c4c4598dd6f1db",
+    "url": "/music-tagger/static/js/main.8c40c848.chunk.js"
+  },
+  {
+    "revision": "5289383b5f0cc5accacb",
+    "url": "/music-tagger/static/js/runtime-main.31226f1e.js"
+  },
+  {
+    "revision": "e15a286155536bf7a2872a2f0846b550",
+    "url": "/music-tagger/static/media/dogs-resnet18.e15a2861.onnx"
+  }
+]);
