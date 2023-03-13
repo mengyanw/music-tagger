373a9c3aacafe77d3ebfeb6ff58da58198402f80
wumengyan <mengyanw@umich.edu>
2023-03-13 18:05:10 -0400

    feat: add router and tabs

--- src/components/App.js
+++ src/components/App.js
@@ -2,9 +2,11 @@ import React from 'react';
 import Classifier from './Classifier'
 import Container from '@material-ui/core/Container';
 import Typography from '@material-ui/core/Typography';
-import Link from '@material-ui/core/Link';
+// import Link from '@material-ui/core/Link';
 import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
 import Toolbar from '@material-ui/core/Toolbar';
 import ExpansionPanel from '@material-ui/core/ExpansionPanel';
 import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
@@ -21,122 +23,52 @@ import Button from '@material-ui/core/Button';
 import breeds from './classes'
 import {getBreed} from './utils'
 
+import { Route, BrowserRouter, Switch, Link } from "react-router-dom";
+import Home from './Home';
+import Video from './Video';
+
 const useStyles = makeStyles(theme => ({
     root: {
         flexGrow: 1,
     },
-    content: {
-        marginTop: 20,
-    },
-    menuButton: {
-        marginRight: theme.spacing(2),
-    },
-    title: {
-        flexGrow: 1,
-    },
-    heading: {
-        fontSize: theme.typography.pxToRem(15),
-        fontWeight: theme.typography.fontWeightRegular,
-    },
-    table: {
-        width: '100%',
-    },
-    footer: {
-        padding: theme.spacing(2),
-        marginTop: 'auto',
-    },
-    button: {
-        margin: theme.spacing(1),
-    },
 }));
 
 function App() {
+    const routes = ["/", "/video"];
     const classes = useStyles();
-    return <div className={classes.root}>
-        <AppBar position="static">
-            <Toolbar>
-                <Typography variant="h6" className={classes.title}>
-                    Music Tagger 
-                </Typography>
-            </Toolbar>
-        </AppBar>
-        <Container className={classes.content}>
-            <Classifier />
-        </Container>
-        <Container className={classes.content}>
-            <ExpansionPanel>
-                <ExpansionPanelSummary
-                expandIcon={<ExpandMoreIcon />}
-                aria-controls="panel1a-content"
-                id="panel1a-header"
-                >
-                <Typography className={classes.heading}>How do I use this?</Typography>
-                </ExpansionPanelSummary>
-                <ExpansionPanelDetails>
-                <Typography variant="body2">
+    return (
+        <div className={classes.root}>
+            <BrowserRouter>
+                <Route
+                    path="/"
+                    render={(history) => (
+                        <AppBar>
+                            <Tabs value={history.location.pathname}>
+                                {console.log(history.location.pathname)}
+                                <Tab
+                                    value="/"
+                                    label="Home"
+                                    component={Link}
+                                    to="/"
+                                />
+                                <Tab
+                                    value="/video"
+                                    label="Video"
+                                    component={Link}
+                                    to="/video"
+                                />
+                            </Tabs>
+                        </AppBar>
+                    )}
+                />
 
-                </Typography>
-                </ExpansionPanelDetails>
-            </ExpansionPanel>
-            <ExpansionPanel>
-                <ExpansionPanelSummary
-                expandIcon={<ExpandMoreIcon />}
-                aria-controls="panel2a-content"
-                id="panel2a-header"
-                >
-                <Typography className={classes.heading}>What music tags does the classifier know?</Typography>
-                </ExpansionPanelSummary>
-                <ExpansionPanelDetails>
-                <Typography variant="body2">
- 
-                </Typography>
-                </ExpansionPanelDetails>
-            </ExpansionPanel>
-            <ExpansionPanel>
-                <ExpansionPanelSummary
-                expandIcon={<ExpandMoreIcon />}
-                aria-controls="panel3a-content"
-                id="panel3a-header"
-                >
-                <Typography className={classes.heading}>What is this good for?</Typography>
-                </ExpansionPanelSummary>
-                <ExpansionPanelDetails>
-                <Typography variant="body2">
-
-                </Typography>
-                </ExpansionPanelDetails>
-            </ExpansionPanel>
-            <ExpansionPanel>
-                <ExpansionPanelSummary
-                expandIcon={<ExpandMoreIcon />}
-                aria-controls="panel3a-content"
-                id="panel3a-header"
-                >
-                <Typography className={classes.heading}>What happens to my data?</Typography>
-                </ExpansionPanelSummary>
-                <ExpansionPanelDetails>
-                <Typography variant="body2">
-
-                </Typography>
-                </ExpansionPanelDetails>
-            </ExpansionPanel>
-            <ExpansionPanel>
-                <ExpansionPanelSummary
-                expandIcon={<ExpandMoreIcon />}
-                aria-controls="panel3a-content"
-                id="panel3a-header"
-                >
-                <Typography className={classes.heading}>Music Data credits</Typography>
-                </ExpansionPanelSummary>
-                <ExpansionPanelDetails>
-
-                </ExpansionPanelDetails>
-            </ExpansionPanel>
-            <footer className={classes.footer}>
-                <Button className={classes.button}>Developed by Mengyan Wu, Yuxiao Liu, Zihui Liu</Button>
-            </footer>
-        </Container>
-  </div>;
+                <Switch>
+                    <Route path="/" component={Home} />
+                    <Route path="/video" component={Video} />
+                </Switch>
+            </BrowserRouter>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/Video.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+function Video() {
+    console.log('Hi')
+    return <h1 style={{ marginTop: "10vh" }}>Videos</h1>;
+}
+
+export default Video;
